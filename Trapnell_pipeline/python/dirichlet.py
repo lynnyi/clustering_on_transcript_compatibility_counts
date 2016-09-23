487f6d7ac838515c69bcb3b7c911ae7233ab48d4
lynnyi <donglinyi@gmail.com>
2016-09-23 10:36:34 -0700

    added likelihood function, made code more efficient

--- Trapnell_pipeline/python/dirichlet.py
+++ Trapnell_pipeline/python/dirichlet.py
@@ -22,7 +22,7 @@ num_samples = num_samples
 def calculate_log_p(ec_counts):
 	log_ec_counts = [map(np.log, x) for x in ec_counts]
 	log_p = map(np.sum, log_ec_counts)
-	log_p = [x / num_samples for x in log_p]
+	log_p /= num_samples
 	return log_p
 
 log_p = calculate_log_p(TCCs)
@@ -31,19 +31,23 @@ assert(len(log_p)==num_ECs)
 #gradient of likelihood at specific point of alpha
 def gradient(alpha):
 	digamma_alpha_sum = sp.digamma(np.sum(alpha))
-	alpha = map(sp.digamma, alpha)	
-	alpha = np.array(log_p) - np.array(alpha)
-	alpha = [(a + digamma_alpha_sum) * num_samples for a in alpha] 
+	alpha = np.array(log_p) - np.array([sp.digamma(x) for x in alpha])
+	alpha += digamma_alpha_sum
+	alpha *= num_samples
+	#alpha = [(x + digamma_alpha_sum) * num_samples for x in alpha] 
 	return alpha
 
 #given old alpha, update old alpha to give new alpha
-def update(old_alpha, step_size):
-	new_alpha = old_alpha + step_size*np.array(gradient(old_alpha))
-	return new_alpha
+def update(alpha, step_size):
+	alpha += step_size*np.array(gradient(alpha))
+	return alpha
 
 #given specific alpha, get likelihood over the dirichlet
 def likelihood(alpha):
-	likelihood = 0
+        log_gamma_sum = np.log(sp.gamma(np.sum(alpha)))
+	sum_log_gamma = np.sum([np.log(sp.gamma(x)) for x in alpha])
+        sum_dot_log_p = np.sum(np.dot([x-1 for x in alpha], log_p))
+	likelihood = num_samples * (log_gamma_sum - sum_log_gamma + sum_dot_log_p)
 	return likelihood
 	
 def gradient_descent(num_steps, step_size):
@@ -51,4 +55,5 @@ def gradient_descent(num_steps, step_size):
 	assert(len(log_p) == len(alpha))
 	for i in range(num_steps):
 		alpha = update(alpha, step_size)
+		print likelihood(alpha)
 	return alpha
